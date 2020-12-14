83a3ac6a763b1e16129cfb15d147337641e48516
TrentaIcedCoffee <jin@ucdavis.edu>
2020-12-13 16:41:50 -0800

    init history, store, router

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Store, createStore, combineReducers, Reducer } from 'redux';
+import { Route, Switch } from 'react-router-dom';
+import {
+  connectRouter,
+  ConnectedRouter,
+  RouterState
+} from 'connected-react-router';
+import { createBrowserHistory, History } from 'history';
+
+interface ApplicationState {
+  router: RouterState;
+}
+
+interface AppProps {
+  store: Store<ApplicationState>;
+  history: History;
+}
+
+function createRootReducer(history: History): Reducer<any, any> {
+  return combineReducers({
+    router: connectRouter(history)
+  });
+}
+
+function configureStore(history: History): Store<ApplicationState> {
+  return createStore(createRootReducer(history));
+}
+
+const history: History = createBrowserHistory();
+
+const store: Store<ApplicationState> = configureStore(history);
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/roadmap" component={() => <div>roadmap</div>} />
+        <Route path="/backlog" component={() => <div>backlog</div>} />
+        <Route path="/sprint" component={() => <div>sprint</div>} />
+        <Route path="/" component={() => <div>Not Found</div>} />
+      </Switch>
+    );
+  }
+}
+
+class App extends React.Component<AppProps> {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Routes />
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App store={store} history={history} />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
